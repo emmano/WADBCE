d710b1a8a3ed34f15bc10645c24ac9a02d36e5d3
emmano <emmanuel.ortiguela@gmail.com>
2014-08-07 19:56:23 -0500

    Deleted UI related elements

--- app/src/main/java/me/emmano/wadbce/DummyActivity.java
+++ app/src/main/java/me/emmano/wadbce/DummyActivity.java
@@ -13,23 +13,4 @@ public class DummyActivity extends Activity {
         super.onCreate(savedInstanceState);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.dummy, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
