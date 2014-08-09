9b1f89b5df39707c73c3409f2a267cdd903626d2
emmano <emmanuel.ortiguela@gmail.com>
2014-08-08 23:35:16 -0500

    FileObserver deletes existing files as well as new files on database folder. Added tests around FilleObserver.

--- app/src/main/java/me/emmano/wadbce/service/WadbceFileObserver.java
+++ app/src/main/java/me/emmano/wadbce/service/WadbceFileObserver.java
@@ -12,14 +12,30 @@ public class WadbceFileObserver extends FileObserver {
 
     private static final String PATH = Environment.getExternalStorageDirectory() + "/WhatsApp/Databases/";
 
+    private final File databases;
+
     public WadbceFileObserver() {
         super(PATH);
-        new File(PATH).delete();
+        databases = new File(PATH);
+        deleteFilesInDirectory();
     }
 
     @Override
-    public void onEvent(int i, String s) {
-        new File(PATH).delete();
+    public void onEvent(int flag, String s) {
+        if (databases.isDirectory() && flag == CREATE) {
+            String[] children = databases.list();
+            for (int j = 0; j < children.length; j++) {
+                new File(databases, children[j]).delete();
+            }
+        }
+    }
 
+    private void deleteFilesInDirectory() {
+        if (databases.isDirectory()) {
+            String[] children = databases.list();
+            for (int i = 0; i < children.length; i++) {
+                new File(databases, children[i]).delete();
+            }
+        }
     }
 }
